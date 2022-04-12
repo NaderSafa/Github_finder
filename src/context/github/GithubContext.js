9ec95a9fef4bd005f361abac28e14e88041b69e2
Nader Safa <cap.nader@gmail.com>
2022-04-12 08:03:16 +0200

    Github_finder

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,31 @@
+import { createContext, useReducer } from 'react'
+import GithubReducer from './GithubReducer'
+
+const GithubContext = createContext()
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+    repos: [],
+  }
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState)
+
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export default GithubContext
